98d22ef494203b7bf682a406224404a6b6d02432
Andy Javier <andy_javier@ucne.edu.do>
2024-11-19 01:32:42 -0400

    migraciones

---
+++ Migrations/20241119050503_InitialCreate.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AndyJavier_AP1_P2.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Combos",
+                columns: table => new
+                {
+                    ComboId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Vendido = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Combos", x => x.ComboId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Costo = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Existencia = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CombosDetalle",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ComboId = table.Column<int>(type: "int", nullable: false),
+                    ProductoId = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    Costo = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CombosDetalle", x => x.DetalleId);
+                    table.ForeignKey(
+                        name: "FK_CombosDetalle_Combos_ComboId",
+                        column: x => x.ComboId,
+                        principalTable: "Combos",
+                        principalColumn: "ComboId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CombosDetalle_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "ProductoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "Costo", "Descripcion", "Existencia", "Precio" },
+                values: new object[,]
+                {
+                    { 1, 105m, "Procesador Ryzen 5 5600X", 8, 300m },
+                    { 2, 82m, "RAM Corsair DDR4 2x16 GB", 10, 200m },
+                    { 3, 100m, "Almacenamiento NVME4.0 1TB Samsung", 15, 200m },
+                    { 4, 305m, "Grafica NVIDIA RTX 4060", 3, 500m },
+                    { 5, 160m, "Case Lian Li Black", 4, 280m },
+                    { 6, 60m, "Mouse Razer", 28, 100m },
+                    { 7, 115m, "Power Supply Redragon 850W 80+G", 5, 280m },
+                    { 8, 160m, "Motherboard Asus Strix B550F", 4, 300m },
+                    { 9, 110m, "Monitor Acer 24p IPS 1080 180hz", 2, 250m },
+                    { 10, 60m, "Teclado Redragon RGB 60% Blue Switches", 8, 100m }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CombosDetalle_ComboId",
+                table: "CombosDetalle",
+                column: "ComboId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CombosDetalle_ProductoId",
+                table: "CombosDetalle",
+                column: "ProductoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CombosDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Combos");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
