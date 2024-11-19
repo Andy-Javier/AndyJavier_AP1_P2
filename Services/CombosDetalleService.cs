0537e7e0a6e4f8f00735c8ba27ce544c72a4547f
Andy Javier <andy_javier@ucne.edu.do>
2024-11-19 01:32:50 -0400

    servicios

---
+++ Services/CombosDetalleService.cs
@@ -0,0 +1,67 @@
+﻿using AndyJavier_AP1_P2.DAL;
+using AndyJavier_AP1_P2.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace AndyJavier_AP1_P2.Services
+{
+    public class CombosDetalleService
+    {
+        private readonly Contexto _contexto;
+
+        public CombosDetalleService(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<List<CombosDetalle>> ObtenerDetallesDeCombo(int comboId)
+        {
+            return await _contexto.CombosDetalle
+                .Include(cd => cd.Combo)
+                .Include(cd => cd.Producto)
+                .Where(cd => cd.ComboId == comboId)
+                .ToListAsync();
+        }
+
+
+        public async Task<CombosDetalle?> ObtenerPorId(int detalleId)
+        {
+            return await _contexto.CombosDetalle
+                .Include(detalle => detalle.Producto)
+                .FirstOrDefaultAsync(detalle => detalle.DetalleId == detalleId);
+        }
+
+        public async Task<CombosDetalle> Crear(CombosDetalle comboDetalle)
+        {
+            await _contexto.CombosDetalle.AddAsync(comboDetalle);
+            await _contexto.SaveChangesAsync();
+            return comboDetalle;
+        }
+
+        public async Task<CombosDetalle> Modificar(CombosDetalle comboDetalle)
+        {
+            _contexto.Entry(comboDetalle).State = EntityState.Modified;
+            await _contexto.SaveChangesAsync();
+            return comboDetalle;
+        }
+
+        public async Task<bool> Eliminar(int detalleId)
+        {
+            var comboDetalle = await _contexto.CombosDetalle.FindAsync(detalleId);
+            if (comboDetalle == null)
+                return false;
+
+            _contexto.CombosDetalle.Remove(comboDetalle);
+            await _contexto.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<List<Productos>> ListaProductos(Expression<Func<Productos, bool>>? criterio = null)
+        {
+            return criterio == null
+                ? await _contexto.Productos.AsNoTracking().ToListAsync()
+                : await _contexto.Productos.Where(criterio).AsNoTracking().ToListAsync();
+        }
+
+    }
+}
---
+++ Services/CombosService.cs
@@ -0,0 +1,60 @@
+﻿using AndyJavier_AP1_P2.DAL;
+using AndyJavier_AP1_P2.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace AndyJavier_AP1_P2.Services
+{
+    public class CombosService
+    {
+        private readonly Contexto _contexto;
+
+        public CombosService(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<List<Combos>> Listar(Expression<Func<Combos, bool>> criterio)
+        {
+            return await _contexto.Combos
+                .Include(combo => combo.CombosDetalles)
+                .ThenInclude(detalle => detalle.Producto)
+                .Where(criterio)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<Combos?> ObtenerPorId(int comboId)
+        {
+            return await _contexto.Combos
+                .Include(combo => combo.CombosDetalles)
+                .ThenInclude(detalle => detalle.Producto)
+                .FirstOrDefaultAsync(combo => combo.ComboId == comboId);
+        }
+
+        public async Task<Combos> Crear(Combos combo)
+        {
+            await _contexto.Combos.AddAsync(combo);
+            await _contexto.SaveChangesAsync();
+            return combo;
+        }
+
+        public async Task<Combos> Modificar(Combos combo)
+        {
+            _contexto.Entry(combo).State = EntityState.Modified;
+            await _contexto.SaveChangesAsync();
+            return combo;
+        }
+
+        public async Task<bool> Eliminar(int comboId)
+        {
+            var combo = await _contexto.Combos.FindAsync(comboId);
+            if (combo == null)
+                return false;
+
+            _contexto.Combos.Remove(combo);
+            await _contexto.SaveChangesAsync();
+            return true;
+        }
+    }
+}
